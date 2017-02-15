0488a8bb59482d41f00b34b747d2d3d699a13e21
Alvaro <alvgaona@gmail.com>
2017-02-15 17:12:40 -0300

    some2

--- NSX_Performance/nsx/conf.py
+++ NSX_Performance/nsx/conf.py
@@ -1,4 +1,4 @@
-nsxraml_file = 'nsxvapi.raml'
+nsxraml_file = 'nsxraml/nsxvapi.raml'
 nsxmanager = '10.120.110.9'
 nsx_username = 'admin'
 nsx_password = 'F1b3rC*rp'
--- NSX_Performance/test.py
+++ NSX_Performance/test.py
@@ -1,4 +1,4 @@
-from conf import *
+from nsx.conf import *
 from nsxramlclient.client import NsxClient
 from nsx.CRUD import Edge, LogicalSwitch
 
@@ -7,10 +7,10 @@ def main():
   session = NsxClient(nsxraml_file, nsxmanager, nsx_username, nsx_password, debug=True)
   edge = Edge(session)
 
-  edge.create(datasourceId='datastore-2458', hostId='host-2436', resourcePoolId='resgroup-152',
+  edge.create(datastoreId='datastore-2458', hostId='host-2436', resourcePoolId='resgroup-2473',
   	username='admin', password='F1b3rC*rp.2017', ip='10.10.10.1', netmask='255.255.255.252', index='0',
-  	mtu='9000', name='Uplink', type='Uplink', portgroupId='', applianceSize='large',
-  	datacenterId='datacenter-', edgeType='gatewayServices', edgeName='Edge001')
+  	mtu='9000', name='Uplink', type='Uplink', portgroupId='dvportgroup-2479', applianceSize='large',
+  	datacenterId='datacenter-2', edgeType='gatewayServices', edgeName='Edge001')
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 	exit(main())
