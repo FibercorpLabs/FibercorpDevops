cedb81c8cc0c0769f0c5a220c7c58747d4f8c38e
alvgaona <alvgaona@gmail.com>
2017-02-15 16:19:02 -0300

    some1

--- NSX_Performance/test.py
+++ NSX_Performance/test.py
@@ -7,10 +7,10 @@ def main():
   session = NsxClient(nsxraml_file, nsxmanager, nsx_username, nsx_password, debug=True)
   edge = Edge(session)
 
-  edge.create(datasourceId='', hostId='', resourcePoolId='',
-  	username='', password='', ip='', netmask='', index='',
-  	mtu='', name='', type='', portgroupId='', applianceSize='',
-  	datacenterId='', edgeType='', edgeName='')
-  
+  edge.create(datasourceId='datastore-2458', hostId='host-2436', resourcePoolId='resgroup-152',
+  	username='admin', password='F1b3rC*rp.2017', ip='10.10.10.1', netmask='255.255.255.252', index='0',
+  	mtu='9000', name='Uplink', type='Uplink', portgroupId='', applianceSize='large',
+  	datacenterId='datacenter-', edgeType='gatewayServices', edgeName='Edge001')
+
 if __name__ = '__main__':
 	exit(main())
