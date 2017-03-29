9ba928b8c67743b3b0a82029316cfde377d3f8a2
alvgaona <alvgaona@gmail.com>
2017-03-29 11:01:52 -0300

    c.o

--- NSX_Performance/run.py
+++ NSX_Performance/run.py
@@ -8,11 +8,11 @@ def main():
 
 	client.set_missing_host_key_policy(AutoAddPolicy())
 	client.connect('200.0.0.2', username='tenant', password='tenant')
-	stdin, stdout, stderr = client.exec_command('sed -i -e "s/Hostname "collectd"/Hostname "collectd1"/g" /etc/collectd/collectd.conf')
+	stdin, stdout, stderr = client.exec_command("sed -i -e 's/Hostname 'collectd'/Hostname 'collectd1'/g' /etc/collectd/collectd.conf")
 
 	server.set_missing_host_key_policy(AutoAddPolicy())
 	server.connect('200.0.0.3', username='tenant', password='tenant')
-	stdin, stdout, stderr = server.exec_command('sed -i -e "s/Hostname "collectd"/Hostname "collectd1"/g" /etc/collectd/collectd.conf')
+	stdin, stdout, stderr = server.exec_command("sed -i -e 's/Hostname 'collectd'/Hostname 'collectd2'/g' /etc/collectd/collectd.conf")
 
 if __name__ == '__main__':
 	exit(main())
