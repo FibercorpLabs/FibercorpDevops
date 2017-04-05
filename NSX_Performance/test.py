a3051909953f840743396b983e7fde0c25b2a83c
alvgaona <alvgaona@gmail.com>
2017-04-05 17:05:42 -0300

    asdasdasda

--- NSX_Performance/test.py
+++ NSX_Performance/test.py
@@ -5,12 +5,12 @@ def main():
 	
 		tenant1 = SSHClient()
 		tenant1.set_missing_host_key_policy(AutoAddPolicy())
-		tenant1.connect('1.1.1.2', username='tenant', password='tenant')
-		stdin, stdout, stderr = tenant1.exec_command("sudo sed -i -e 's/hosthost/host2/g' /etc/collectd/collectd.conf", get_pty=True)
+		tenant1.connect('1.1.1.46', username='tenant', password='tenant')
+		stdin, stdout, stderr = tenant1.exec_command("sudo sed -i -e 's/hosthost/host46/g' /etc/collectd/collectd.conf", get_pty=True)
 		stdin.write('tenant\n')
 		stdin.flush()		
 
-		stdin, stdout, stderr = tenant1.exec_command("sudo sed -i -e 's/200.0.0.252/1.1.1.252/g' /etc/collectd/collectd.conf", get_pty=True)
+		stdin, stdout, stderr = tenant1.exec_command("sudo sed -i -e 's/1.1.1.25245/1.1.1.252/g' /etc/collectd/collectd.conf", get_pty=True)
 		stdin.write('tenant\n')
 		stdin.flush()
 
