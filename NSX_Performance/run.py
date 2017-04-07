fb7203e04e6d9e20bea8dbb49e7960c8a9b47715
alvgaona <alvgaona@gmail.com>
2017-04-07 11:42:36 -0300

    10m 1800s test

--- NSX_Performance/run.py
+++ NSX_Performance/run.py
@@ -24,10 +24,10 @@ def main():
 		stdin, stdout, stderr = tenant2.exec_command("nuttcp -S -p 6666")
 		stdin, stdout, stderr = tenant2.exec_command("nuttcp -S -p 6667")
 
-		stdin, stdout, stderr = tenant1.exec_command("nuttcp -b -v -I ens192 -l8192 -N 1 -p 6666 -R30 -ws 8m -M 1500 -T 3600 192.168.1.2")
-		stdin, stdout, stderr = tenant2.exec_command("nuttcp -b -v -I ens192 -l8192 -N 1 -p 6666 -R30 -ws 8m -M 1500 -T 3600 192.168.0.2")
-		stdin, stdout, stderr = tenant1.exec_command("nuttcp -u -b -v -I ens192 -l8192 -N 1 -p 6667 -R30 -ws 8m -T 3600 192.168.1.2")
-		stdin, stdout, stderr = tenant2.exec_command("nuttcp -u -b -v -I ens192 -l8192 -N 1 -p 6667 -R30 -ws 8m -T 3600 192.168.0.2")
+		stdin, stdout, stderr = tenant1.exec_command("nuttcp -b -v -I ens192 -l8192 -N 1 -p 6666 -R10 -ws 8m -M 1500 -T 1800 192.168.1.2")
+		stdin, stdout, stderr = tenant2.exec_command("nuttcp -b -v -I ens192 -l8192 -N 1 -p 6666 -R10 -ws 8m -M 1500 -T 1800 192.168.0.2")
+		stdin, stdout, stderr = tenant1.exec_command("nuttcp -u -b -v -I ens192 -l8192 -N 1 -p 6667 -R10 -ws 8m -T 1800 192.168.1.2")
+		stdin, stdout, stderr = tenant2.exec_command("nuttcp -u -b -v -I ens192 -l8192 -N 1 -p 6667 -R10 -ws 8m -T 1800 192.168.0.2")
 		
 		print "Test %s is running" % str(i)
 		print "Test %s is running" % str(i+1)
