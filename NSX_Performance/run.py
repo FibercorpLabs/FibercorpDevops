154d2533d289cfab3043fd74eb8e9aaff33fe8b6
alvgaona <alvgaona@gmail.com>
2017-04-07 11:37:20 -0300

    ,,,

--- NSX_Performance/run.py
+++ NSX_Performance/run.py
@@ -1,4 +1,5 @@
 from paramiko import SSHClient, AutoAddPolicy
+from time import sleep
 
 def main():
 
@@ -18,23 +19,18 @@ def main():
 		tenant2.set_missing_host_key_policy(AutoAddPolicy())
 		tenant2.connect('1.1.1.%s' % str((i+1)), username='tenant', password='tenant')
 			
-		#stdin, stdout, stderr = tenant2.exec_command("sudo service collectd restart", get_pty=True)
-		#stdin.write('tenant\n')
-		#stdin.flush()
-
-		#print "Restarted: %s" % str(i+1)
-
 		stdin, stdout, stderr = tenant1.exec_command("nuttcp -S -p 6666")
 		stdin, stdout, stderr = tenant1.exec_command("nuttcp -S -p 6667")
 		stdin, stdout, stderr = tenant2.exec_command("nuttcp -S -p 6666")
 		stdin, stdout, stderr = tenant2.exec_command("nuttcp -S -p 6667")
 
-		stdin, stdout, stderr = tenant1.exec_command("nuttcp -b -v -I ens192 -l8192 -N 1 -p 6666 -Ru -ws 8m -M 1500 -T 3600 192.168.1.2")
-		stdin, stdout, stderr = tenant2.exec_command("nuttcp -b -v -I ens192 -l8192 -N 1 -p 6666 -Ru -ws 8m -M 1500 -T 3600 192.168.0.2")
-		stdin, stdout, stderr = tenant1.exec_command("nuttcp -u -b -v -I ens192 -l8192 -N 1 -p 6667 -Ru -ws 8m -T 3600 192.168.1.2")
-		stdin, stdout, stderr = tenant2.exec_command("nuttcp -u -b -v -I ens192 -l8192 -N 1 -p 6667 -Ru -ws 8m -T 3600 192.168.0.2")
+		stdin, stdout, stderr = tenant1.exec_command("nuttcp -b -v -I ens192 -l8192 -N 1 -p 6666 -R30 -ws 8m -M 1500 -T 3600 192.168.1.2")
+		stdin, stdout, stderr = tenant2.exec_command("nuttcp -b -v -I ens192 -l8192 -N 1 -p 6666 -R30 -ws 8m -M 1500 -T 3600 192.168.0.2")
+		stdin, stdout, stderr = tenant1.exec_command("nuttcp -u -b -v -I ens192 -l8192 -N 1 -p 6667 -R30 -ws 8m -T 3600 192.168.1.2")
+		stdin, stdout, stderr = tenant2.exec_command("nuttcp -u -b -v -I ens192 -l8192 -N 1 -p 6667 -R30 -ws 8m -T 3600 192.168.0.2")
 		
 		print "Test %s is running" % str(i)
+		print "Test %s is running" % str(i+1)
 
 		sleep(0.5)
 
