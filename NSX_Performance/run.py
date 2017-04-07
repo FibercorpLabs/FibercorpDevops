ab40b7b27f6afcab4e5c727cac95b32dd045ecc9
alvgaona <alvgaona@gmail.com>
2017-04-07 11:49:14 -0300

    10m 1800s test v0.2

--- NSX_Performance/run.py
+++ NSX_Performance/run.py
@@ -10,29 +10,29 @@ def main():
 		tenant1.set_missing_host_key_policy(AutoAddPolicy())
 		tenant1.connect('1.1.1.%s' % str(i), username='tenant', password='tenant')
 		
-		#stdin, stdout, stderr = tenant1.exec_command("sudo service collectd restart", get_pty=True)
-		#stdin.write('tenant\n')
-		#stdin.flush()
-
-		#print "Restarted: %s" % str(i)
-		
 		tenant2.set_missing_host_key_policy(AutoAddPolicy())
 		tenant2.connect('1.1.1.%s' % str((i+1)), username='tenant', password='tenant')
 			
 		stdin, stdout, stderr = tenant1.exec_command("nuttcp -S -p 6666")
+		sleep(0.1)
 		stdin, stdout, stderr = tenant1.exec_command("nuttcp -S -p 6667")
+		sleep(0.1)
 		stdin, stdout, stderr = tenant2.exec_command("nuttcp -S -p 6666")
+		sleep(0.1)
 		stdin, stdout, stderr = tenant2.exec_command("nuttcp -S -p 6667")
+		sleep(0.1)
 
 		stdin, stdout, stderr = tenant1.exec_command("nuttcp -b -v -I ens192 -l8192 -N 1 -p 6666 -R10 -ws 8m -M 1500 -T 1800 192.168.1.2")
 		stdin, stdout, stderr = tenant2.exec_command("nuttcp -b -v -I ens192 -l8192 -N 1 -p 6666 -R10 -ws 8m -M 1500 -T 1800 192.168.0.2")
 		stdin, stdout, stderr = tenant1.exec_command("nuttcp -u -b -v -I ens192 -l8192 -N 1 -p 6667 -R10 -ws 8m -T 1800 192.168.1.2")
 		stdin, stdout, stderr = tenant2.exec_command("nuttcp -u -b -v -I ens192 -l8192 -N 1 -p 6667 -R10 -ws 8m -T 1800 192.168.0.2")
 		
+		sleep(1)
+
 		print "Test %s is running" % str(i)
 		print "Test %s is running" % str(i+1)
 
-		sleep(0.5)
+		
 
 		i += 2
 		if i == 122:
