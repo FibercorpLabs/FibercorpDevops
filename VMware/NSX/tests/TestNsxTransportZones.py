1979c061ec26c0d7fc2a75ff1928e18d0681eea1
alvgaona <alvgaona@gmail.com>
2017-10-31 12:19:15 -0300

    Transport Zones unit tests done

--- VMware/NSX/tests/TestNsxTransportZones.py
+++ VMware/NSX/tests/TestNsxTransportZones.py
@@ -1,49 +1,67 @@
 # tests/TestNsxLogicalSwitches.py
 import unittest
 import sys
-sys.path.append("../utils/")
+sys.path.append("../utils/nsx/")
 from transportzone import *
 from pprint import pprint
 
-
-
 class NsxTransportZoneTestCase(unittest.TestCase):
-    def __init__(self):
-        self.tzId = ""
-
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    def test_getAllTransportZones(self):
-        tzones = getAllTZ()
+    def test_01_getAllTransportZones(self):
+        tzones = getAllTzId()
         self.assertTrue(tzones is not None)
-        pprint(tzones)
-
     
-    def test_getTransportZoneByName(self):
+    def test_02_getTransportZoneByName(self):
         name = "GLOBAL-TZ-LAB"
-        tzName, tzId = getTZ(name)
+
+        tzName, tzId = getTzIdByName(name)
         self.assertEqual(name, tzName)
         self.assertTrue(tzId is not None)
+        print tzName, tzId
 
-    @unittest.skip("Skipped")
-    def test_createTransportZone(self):
+    @unittest.skip("")
+    def test_03_getTransportZoneById(self):
         pass
 
+    def test_04_createTransportZone(self):
+        name = "TZ-TEST"
+        clusters = [{'objectId' : 'domain-c444'}]
+        description = "Transport Zone Create Test"
+        controlPlaneMode = "HYBRID_MODE"
 
-    @unittest.skip("Skipped")
-    def test_updateTransportZone(self):
-        pass
+        createTZ(name, clusters, description, controlPlaneMode)
 
-    @unittest.skip("Skipped")
-    def test_deleteTransportZone(self):
-        pass
+        tzName, tzId = getTzIdByName(name)
+
+        self.assertEqual(name, tzName)
+        self.assertTrue(tzId is not None)  
+
+    def test_05_updateTransportZone(self):
+        name = "TZ-TEST"
+        newName = "TZ-TEST-NEW"
+        clusters = [{'objectId' : 'domain-c444'}]
+       
+        updateTzByName(name, clusters, newName)
+
+        tzName, tzId = getTzIdByName(newName)
+        self.assertEqual(tzName, newName)
+
+    
+    def test_06_deleteTransportZone(self):
+        name = "TZ-TEST-NEW"
+        deleteTzByName(name)
+
+        tzName, tzId = getTzIdByName(name)
+        
+        self.assertEqual(tzName, None)
 
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite = unittest.TestLoader().loadTestsFromTestCase(NsxLogicalSwitchesTestCase)
+    suite = unittest.TestLoader().loadTestsFromTestCase(NsxTransportZoneTestCase)
     unittest.TextTestRunner(verbosity=2).run(suite)
