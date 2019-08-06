49f62a9d1c032d924ec67f2b0299561a830e9ef5
arielik <arielik@gmail.com>
2019-08-06 13:48:10 -0300

    organize repo distribution

--- vmware/vCenter/applyAffinityRules.py
+++ vmware/vCenter/applyAffinityRules.py
@@ -5,14 +5,21 @@ import requests
 import argparse
 from VMWConfigFile import *
 from pyVim import connect
+from pyVim.connect import SmartConnect, Disconnect
 from pyVmomi import vmodl
 from pyVmomi import vim
+import os
+import sys
+import time
+import getpass
 
 # Disabling urllib3 ssl warnings
 requests.packages.urllib3.disable_warnings()
 
 # Disabling SSL certificate verification
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+#context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+
 context.verify_mode = ssl.CERT_NONE
 
 
@@ -36,18 +43,11 @@ def get_obj(content, vimtype, name):
     return obj
 
 
-# # find your cluster, I'm going to cheat for brevity
-# cluster = si.content.rootFolder.childEntity[0].hostFolder.childEntity[0]
-# get the VMs you are interested in
-vms = [cluster.host[0].vm[0], cluster.host[1].vm[0]]
-# spec ref: https://www.vmware.com/support/developer/vc-sdk/visdk41pubs/ApiReference/vim.cluster.AffinityRuleSpec.html
-# extends: https://www.vmware.com/support/developer/vc-sdk/visdk41pubs/ApiReference/vim.cluster.RuleInfo.html
-# Note that you need to set a name.
-rule = vim.cluster.AffinityRuleSpec(vm=vms, enabled=True, mandatory=True,
-                                    name='affinity-between-2-vms')
-ruleSpec = vim.cluster.RuleSpec(info=rule, operation='add')
-configSpec = vim.cluster.ConfigSpecEx(rulesSpec=[ruleSpec])
-WaitForTask(cluster.ReconfigureEx(configSpec, modify=True))
+def get_vim_objects(content, vim_type):
+    '''Get vim objects of a given type.'''
+    return [item for item in content.viewManager.CreateContainerView(
+        content.rootFolder, [vim_type], recursive=True
+    ).view]
 
 
 def main():
@@ -72,7 +72,7 @@ def main():
                                       port=443,
                                       sslContext=context)
 
-        except IOError, e:
+        except IOError as e:
             pass
             atexit.register(Disconnect, si)
 
@@ -81,30 +81,36 @@ def main():
         # Load JSON in dict
 
         # Iterate all VMs
+        for vm in get_vim_objects(content, vim.VirtualMachine):
+            if not vm.config == None:
+                if not vm.config.template:
+                    if vm.customValue:
+                        for i in vm.customValue:
+                            print("VM Custom Value: ", i.key)
 
-        # if VM matches custom attribute in dict
-        if vm matches:
-            # Find Cluster for the VM
-            cluster = get_obj(
-                content, [vim.ClusterComputeResource], args.cluster)
+                    # if VM matches custom attribute in dict
+                    # if vm.name:
+                    #     # Find Cluster for the VM
+                    #     cluster = get_obj(
+                    #         content, [vim.ClusterComputeResource], args.cluster)
 
-            # Find affinity rule
-            rule = get_obj(
-                content, [vim.cluster.AffinityRuleSpec], affinityRule)
+                    #     # Find affinity rule
+                    #     rule = get_obj(
+                    #         content, [vim.cluster.AffinityRuleSpec], affinityRule)
 
-            # create rule spec & config spec
-            ruleSpec = vim.cluster.RuleSpec(info=rule, operation='add')
-            configSpec = vim.cluster.ConfigSpecEx(rulesSpec=[ruleSpec])
+                    #     # create rule spec & config spec
+                    #     ruleSpec = vim.cluster.RuleSpec(info=rule, operation='add')
+                    #     configSpec = vim.cluster.ConfigSpecEx(rulesSpec=[ruleSpec])
 
-            # apply AR
-            WaitForTask(cluster.ReconfigureEx(configSpec, modify=True))
+                    #     # apply AR
+                    #     WaitForTask(cluster.ReconfigureEx(configSpec, modify=True))
 
-    except vmodl.MethodFault, e:
-        print "Caught vmodl fault: %s" % e.msg
+    except vmodl.MethodFault as e:
+        print("Caught vmodl fault: %s" % e.msg)
         return 1
 
-    except Exception, e:
-        print "Caught exception: %s" % str(e)
+    except Exception as e:
+        print("Caught exception: %s" % str(e))
         return 1
 
 
